bd389d858c7079471d86866d680cb7b6695cc3f6
Ruth M. Pardee <ruthmpardee@gmail.com>
2016-06-07 15:18:50 -0700

    Dates formatted correctly

--- client/app.js
+++ client/app.js
@@ -1,10 +1,23 @@
 
-// $(function(){
+// DATE HELPERS
 
-  // var app = new AppView();
+var monthNames = [
+  "January", "February", "March",
+  "April", "May", "June", "July",
+  "August", "September", "October",
+  "November", "December"
+];
 
-  // var app = new AppModel({model: app});
+var formatDate = function(date) {
+  var day = date.getDate();
+  var monthIndex = date.getMonth();
+  var year = date.getFullYear();
 
-  // put the view onto the screen
-  // $('body').append(app.render());
-// });
+  return monthNames[monthIndex] + ' ' + day + ', ' + year;
+};
+
+var testDate = 'Tue Jun 14 2016 15:11:35 GMT-0700 (PDT)';
+// console.log("testDate: ", formatDate(testDate));
+var today = new Date();
+console.log("today's date: ", formatDate(today));
+
--- client/views/ScheduleEntryView.js
+++ client/views/ScheduleEntryView.js
@@ -5,6 +5,11 @@ var ScheduleEntryView = Backbone.View.extend({
   template: _.template('<td><%= name %></td><td><%= date %></td>'),
 
   render: function() {
-    return this.$el.html(this.template(this.model.attributes));
+    // var attribs = this.model.attributes;
+    var formattedAttribs = {
+      name: this.model.attributes.name,
+      date: formatDate(this.model.attributes.date)
+    }
+    return this.$el.html(this.template(formattedAttribs));
   }
 });
